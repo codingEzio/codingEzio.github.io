4ab678a6981faf34c42340dee3637d518682da65
Nemo <assassinste@gmail.com>
2024-07-27 09:07:10 +0800

    feat: Notes and examples for signals on topic Operating System

--- hands-on/mock-signal.c
+++ hands-on/mock-signal.c
@@ -1,6 +1,7 @@
 #include <signal.h>
 #include <stdio.h>
 #include <signal.h>
+#include <sys/signal.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,27 +10,24 @@
 void signal_handler(int signum) {
     printf("[received signal] %s\n", strsignal(signum));
 
-    if (signum == SIGQUIT) {
+    if (signum == SIGQUIT || signum == SIGINT) {
         printf("Terminating...\n");
         exit(0);
     }
 }
 
 int main() {
-    // Ctrl+C
-    // Ctrl+Z
+    // Ctrl+\ or Ctrl+C
+    signal(SIGQUIT, signal_handler);
     signal(SIGINT, signal_handler);
-    signal(SIGTERM, signal_handler);
 
     // kill -USR1 <pid>
     // kill -USR2 <pid>
     signal(SIGUSR1, signal_handler);
     signal(SIGUSR2, signal_handler);
 
-    signal(SIGQUIT, signal_handler);
-
     while(1) {
-        printf("[pid: %d] [ctrl+\\ to exit] \n", getpid());
+        printf("Press Ctrl+\\ or Ctrl+C to exit...(pid: %d)\n", getpid());
         sleep(2);
     }
 
